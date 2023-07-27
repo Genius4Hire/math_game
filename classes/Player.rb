eb5a1242aee83467a2421ec0ace5249f87c738a0
Jordan Dennis <jordan.dennis@gmail.com>
2023-07-27 04:00:34 +0000

    Simple end of life actions for player

--- classes/Player.rb
+++ classes/Player.rb
@@ -6,4 +6,12 @@ class Player
     @score = 0;
   end
 
+  def die
+    puts "Your math skills failed you for the last time, you lose!"
+  end
+
+  def rejoice
+    puts "You defeated your opponent!!  RRAAAAwwRR!"
+  end
+
 end
